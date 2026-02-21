965f9571593106ba856ca2802e0048d43b112da4
Oderiodo085 <oderiodoamaraldiasfilho19@gmail.com>
2026-02-21 15:10:15 +0000

    Aula 1

---
+++ calculadora/soma.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+    int a, b, soma;
+
+    cout << "Qual o primeiro número ";
+    cin >> a;
+
+    cout << "Qual o segundo número ";
+    cin >> b;
+
+    soma = a + b;
+
+    cout << "A soma entre " << a << " e " << b << " é igual a: " << soma << "\n";
+    
+    
+    return 0;
+}
