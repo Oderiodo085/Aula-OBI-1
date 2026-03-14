191e36d03e4e91bc0d26eae70bd30e8a1f16a120
Oderiodo085 <oderiodoamaraldiasfilho19@gmail.com>
2026-03-14 14:39:38 +0000

    Aula 4

---
+++ vetor/vetor.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(void){
+    int n;
+
+    cout << "Quantos inteiros você vai escrever?";
+    cin >> n;
+
+    int vector[n];
+
+    for(int i = 0; i < n; i++){
+        cin >> v[i];
+    }
+
+    for()
+    cout << i;
+    return 0;
+}
